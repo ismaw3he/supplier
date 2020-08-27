e5f13b348bb637e8a7852e12ef3e800560d81729
Code_Father <rauf.raymond110@gmail.com>
2020-08-27 04:05:04 +0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import LogIn from "./components/logIn/LogIn"
 import SignUp from "./components/signUp/SignUp"
 import Main from "./components/main/Main"
+
 
 import { Provider } from "react-redux";
 import store from "./redux/store"
@@ -10,11 +18,35 @@ import store from "./redux/store"
 function App() {
   return (
     <Provider store={store} >
-      <div className="App">
-        {/* <LogIn /> */}
-        <SignUp />
-        {/* <Main/> */}
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/logIn">Log In</Link>
+              </li>
+              <li>
+                <Link to="/signUp">Sign Up</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+          <Route exact path="/">
+              <Main />
+            </Route>
+            <Route  path="/signUp">
+              <SignUp />
+            </Route>
+            <Route path="/logIn">
+              <LogIn />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 }
